ad6299dbea61b46ea1bf23dfbb1702ca144cec66
GregKnott <46542835+GregKnott@users.noreply.github.com>
2021-04-02 15:17:13 -0400

    Created base UI for home, and user info pages as well as connected controllers for them

---
+++ Momentum Fitness/Controllers/HomeVC.swift
@@ -0,0 +1,31 @@
+//
+//  HomeVC.swift
+//  Momentum Fitness
+//
+//  Created by Greg Knott on 2021-04-02.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //Adds the logo to the top center, as well as removing text from back button, needs to be on every VC
+       styleTopBar(nav: navigationItem)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
