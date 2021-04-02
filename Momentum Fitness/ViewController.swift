cbe06b311814cf2772148e9f02ac666b1aac8b1b
GregKnott <46542835+GregKnott@users.noreply.github.com>
2021-04-02 08:45:50 -0400

    Initial Commit

---
+++ Momentum Fitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Momentum Fitness
+//
+//  Created by Greg Knott on 2021-04-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
