0a4a4e60aee5127c24ef813cfcbbebc75d5afcc4
Malcolm Busari <malcolmbusari18@gmail.com>
2021-04-02 21:42:26 -0700

    Created the "User Profile" page and workout pages to create a new workout from activities. Added "Next" buttons to the user info pages

---
+++ Momentum Fitness/Controllers/WorkoutRoutineVC.swift
@@ -0,0 +1,30 @@
+//
+//  WorkoutRoutineVC.swift
+//  Momentum Fitness
+//
+//  Created by  on 4/2/21.
+//
+
+import UIKit
+
+class WorkoutRoutineVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Adds the logo to the top center, as well as removing text from back button, needs to be on every VC
+        styleTopBar(nav: navigationItem)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
