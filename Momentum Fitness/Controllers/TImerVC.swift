84a657f34c3677c97270513af89236bf5dbd0d49
malcolmbus <malcolmbusari18@mail.com>
2021-11-27 11:47:43 -0800

    Added Timer feature in Storyboard (non-functional)

---
+++ Momentum Fitness/Controllers/TImerVC.swift
@@ -0,0 +1,29 @@
+//
+//  TImerVC.swift
+//  Momentum Fitness
+//
+//  Created by  on 2021-11-27.
+//
+
+import UIKit
+
+class TImerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
